5c25bcb832bf10dba447e6fadbe03af3e37c87df
fyk <fyk943749465@gmail.com>
2023-06-23 22:12:05 +0800

    处理客户端端口的异常

--- netty-demo/src/test/java/com/feng/netty/selector/Server.java
+++ netty-demo/src/test/java/com/feng/netty/selector/Server.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.Iterator;
+
+import static com.feng.netty.c1.utils.ByteBufferUtil.debugAll;
 
 @Slf4j
 public class Server {
@@ -30,11 +33,28 @@ public class Server {
             while (iter.hasNext()) {
                 SelectionKey key = iter.next();
                 log.debug("Key: {}", key);
-                ServerSocketChannel channel = (ServerSocketChannel)key.channel();
-                SocketChannel sc = channel.accept();  // 客户端的channel
-                log.debug("{}", sc);
+                if (key.isAcceptable()) {
+                    ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+                    SocketChannel sc = channel.accept();  // 客户端的channel
+                    sc.configureBlocking(false);
+                    log.debug("{}", sc);
+                    SelectionKey scKey = sc.register(selector, 0, null); // 将 sc 与 selector 关联
+                    scKey.interestOps(SelectionKey.OP_READ); // 在 sc 上注册刚兴趣的事件
+                } else if (key.isReadable()) {
+                    // 捕获异常
+                    try {
+                        SocketChannel socketChannel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(16);
+                        socketChannel.read(buffer);
+                        buffer.flip();
+                        debugAll(buffer);
+                    } catch (IOException e) {
+                       e.printStackTrace();
+                       key.cancel(); // 发生异常，将感兴趣的事件取消掉（也是一种处理方式），事件必须处理
+                    }
+                }
+                iter.remove(); // 这行代码很关键，收到感兴趣的事件后，要从 selectedKeys 中删除，避免重复处理，因为selector不会自动删除
             }
         }
-
     }
 }
