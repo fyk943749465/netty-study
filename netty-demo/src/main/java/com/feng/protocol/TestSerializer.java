a2f738e59c4b49216fcd94968874de2dd3c177e1
fyk <fyk943749465@gmail.com>
2023-06-27 17:56:53 +0800

    fix

--- netty-demo/src/main/java/com/feng/protocol/TestSerializer.java
+++ netty-demo/src/main/java/com/feng/protocol/TestSerializer.java
@@ -1,6 +1,10 @@
 package com.feng.protocol;
 
+import com.feng.config.Config;
 import com.feng.message.LoginRequestMessage;
+import com.feng.message.Message;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.logging.LoggingHandler;
 
@@ -13,6 +17,32 @@ public class TestSerializer {
         EmbeddedChannel channel = new EmbeddedChannel(LOGGING, CODEC, LOGGING);
 
         LoginRequestMessage message = new LoginRequestMessage("zhangsan", "123");
-        channel.writeOutbound(message);
+
+        // channel.writeOutbound(message); // 测试出站的代码
+
+        // 测试入站代码
+        ByteBuf buf = messageToByteBuf(message);
+        channel.writeInbound(buf);
+    }
+
+
+    /**
+     * 测试入站
+     * @param msg
+     * @return
+     */
+    public static ByteBuf messageToByteBuf(Message msg) {
+        int algorithm = Config.getSerializerAlgorithm().ordinal();
+        ByteBuf out = ByteBufAllocator.DEFAULT.buffer();
+        out.writeBytes(new byte[]{1, 2, 3, 4});
+        out.writeByte(1);
+        out.writeByte(algorithm);
+        out.writeByte(msg.getMessageType());
+        out.writeInt(msg.getSequenceId());
+        out.writeByte(0xff);
+        byte[] bytes = Serializer.Algorithm.values()[algorithm].serialize(msg);
+        out.writeInt(bytes.length);
+        out.writeBytes(bytes);
+        return out;
     }
 }
